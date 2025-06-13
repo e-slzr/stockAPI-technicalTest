eea65637f36b81dec53c0630e2909591fc8ec51b
eliu.asalazar <65575720+e-slzr@users.noreply.github.com>
2025-06-13 10:02:18 -0600

    feat: add BoxesController

---
+++ StockAPI/Controllers/BoxesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockAPI.Data;
+using StockAPI.Models;
+
+namespace StockAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoxesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BoxesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/boxes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Box>>> GetBoxes()
+        {
+            return await _context.Boxes
+                .Include(b => b.Transactions) // Incluye las transacciones de cada caja
+                .ToListAsync();
+        }
+
+        // GET: api/boxes/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Box>> GetBox(int id)
+        {
+            var box = await _context.Boxes
+                .Include(b => b.Transactions)
+                    .ThenInclude(t => t.Product)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (box == null)
+            {
+                return NotFound();
+            }
+
+            return box;
+        }
+    }
+}
